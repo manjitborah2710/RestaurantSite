b7a6068f938a248479381935653ca4683afd0942
-v <manjitborah2710@gmail.com>
2020-07-03 23:24:57 +0530

    added onhover display name

--- js/about.js
+++ js/about.js
@@ -1,2 +1,15 @@
 var nameHolder=document.getElementById("name-holder");
-consol
+console.log(nameHolder.id+"");
+var profiles=document.getElementsByClassName("profile-img");
+var name="";
+for(var i=0;i<profiles.length;i++){
+    console.log(profiles[i].className)
+    profiles[i].onmouseover=displayName;
+    profiles[i].onmouseout=removeName;
+}
+function displayName(e){
+    nameHolder.innerHTML=e.target.name+"<br>"+"_________";
+}
+function removeName(e){
+    nameHolder.innerHTML="";
+}
